d51c78ee046cd36afbd1b9078934c6737ca4e209
DavDavUdav <stepan05az@gmail.com>
2026-02-07 23:03:17 +0300

    refactor(console): extract log level to color mapping

--- ULog.Console/ConsoleLogger.cs
+++ ULog.Console/ConsoleLogger.cs
@@ -5,27 +5,11 @@ namespace ULog.Console
 {
     public class ConsoleLogger : ILogger
     {
-        public void Log(LogLevel logLevel, string message)
+        public void Log(LogLevel logLevel, string msg)
         {
-            System.Console.ForegroundColor = GetColor(logLevel);
-            System.Console.WriteLine(message);
+            System.Console.ForegroundColor = logLevel.ToConsoleColor();
+            System.Console.WriteLine(MessageBuilder.Build(msg, logLevel));
             System.Console.ForegroundColor = ConsoleColor.Gray;
-        }
-
-        private ConsoleColor GetColor(LogLevel logLevel)
-        {
-            switch (logLevel)
-            {
-                case LogLevel.Debug:
-                    return ConsoleColor.Gray;
-                case LogLevel.Info:
-                    return ConsoleColor.Green;
-                case LogLevel.Warn:
-                    return ConsoleColor.Yellow;
-                case LogLevel.Error:
-                    return ConsoleColor.Red;
-            }
-            return ConsoleColor.Gray;
         }
     }
 }
---
+++ ULog.Console/LogLevelExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULog.Console
+{
+    public static class LogLevelExtensions
+    {
+        public static ConsoleColor ToConsoleColor(this LogLevel level)
+        {
+            return level switch
+            {
+                LogLevel.Debug => ConsoleColor.Yellow,
+                LogLevel.Info => ConsoleColor.Green,
+                LogLevel.Warn => ConsoleColor.Yellow,
+                LogLevel.Error => ConsoleColor.Red,
+            };
+        }
+    }
+}
