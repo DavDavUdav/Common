ff680a7c8e8ba4149cc13d6602e53c4bd775304c
DavDavUdav <stepan05az@gmail.com>
2026-02-07 12:18:41 +0300

    feat: add console logger

---
+++ ULog.Console/ConsoleLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ulog.Interfaces;
+
+namespace ULog.Console
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(LogLevel logLevel, string message)
+        {
+            System.Console.ForegroundColor = GetColor(logLevel);
+            System.Console.WriteLine(message);
+            System.Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private ConsoleColor GetColor(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Debug:
+                    return ConsoleColor.Gray;
+                case LogLevel.Info:
+                    return ConsoleColor.Green;
+                case LogLevel.Warn:
+                    return ConsoleColor.Yellow;
+                case LogLevel.Error:
+                    return ConsoleColor.Red;
+            }
+            return ConsoleColor.Gray;
+        }
+    }
+}
