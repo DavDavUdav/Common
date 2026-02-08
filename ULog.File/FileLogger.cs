56e3b7ead9aad8e28b91a8d0ca92951feaac8c02
DavDavUdav <stepan05az@gmail.com>
2026-02-08 15:43:14 +0300

    feat(ULog.File): add file logger

---
+++ ULog.File/FileLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+using Ulog.Interfaces;
+
+namespace ULog.File
+{
+    public class FileLogger : ILogger
+    {
+        private FileSettings _settings;
+
+        private string _path;
+
+        public FileLogger(FileSettings settings)
+        {
+            _settings = settings;
+            
+            Directory.CreateDirectory(_settings.Path);
+            
+            _path = Path.Combine(_settings.Path, _settings.FileName);
+        }
+
+        public void Log(LogLevel logLevel, string message)
+        {
+            System.IO.File.AppendAllText(_path, message + "\n", Encoding.UTF8);
+        }
+    }
+}
---
+++ ULog.File/FileSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ULog.File
+{
+    public class FileSettings
+    {
+        public string FileName { get; set; }
+        public string Path { get; set; }
+    }
+}
